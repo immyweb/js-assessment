d11f545432d0bae1f2ca1f59ed67f84d1212dd1a
Imran Oozeerally <>
2025-07-10 16:16:19 +0100

    Generator answers

--- src/answers/generators.js
+++ src/answers/generators.js
@@ -16,7 +16,13 @@
  * [...countTo(5)] // [1, 2, 3, 4, 5]
  * [...countTo(3)] // [1, 2, 3]
  */
-export function* countTo(n) {}
+export function* countTo(n) {
+  let index = 1;
+
+  while (index <= n) {
+    yield index++;
+  }
+}
 
 /**
  * Write a generator function that yields even numbers up to a limit.
@@ -24,7 +30,11 @@ export function* countTo(n) {}
  * [...evenNumbers(10)] // [2, 4, 6, 8, 10]
  * [...evenNumbers(5)] // [2, 4]
  */
-export function* evenNumbers(limit) {}
+export function* evenNumbers(limit) {
+  for (let i = 2; i <= limit; i += 2) {
+    yield i;
+  }
+}
 
 /**
  * Write a generator function that repeats a value n times.
@@ -32,7 +42,11 @@ export function* evenNumbers(limit) {}
  * [...repeat('hello', 3)] // ['hello', 'hello', 'hello']
  * [...repeat('x', 2)] // ['x', 'x']
  */
-export function* repeat(value, times) {}
+export function* repeat(value, times) {
+  for (let i = 0; i < times; i++) {
+    yield value;
+  }
+}
 
 /**
  * Write a generator function that yields items from an array one by one.
@@ -40,7 +54,11 @@ export function* repeat(value, times) {}
  * [...arrayGenerator([1, 2, 3])] // [1, 2, 3]
  * [...arrayGenerator(['a', 'b'])] // ['a', 'b']
  */
-export function* arrayGenerator(array) {}
+export function* arrayGenerator(array) {
+  for (let i = 0; i < array.length; i++) {
+    yield array[i];
+  }
+}
 
 /**
  * Write a generator function that yields the first n Fibonacci numbers.
@@ -48,7 +66,17 @@ export function* arrayGenerator(array) {}
  * [...fibonacci(5)] // [0, 1, 1, 2, 3]
  * [...fibonacci(7)] // [0, 1, 1, 2, 3, 5, 8]
  */
-export function* fibonacci(n) {}
+export function* fibonacci(n) {
+  let previous = 0;
+  let current = 1;
+  let yieldCount = 0;
+
+  while (yieldCount < n) {
+    yield previous;
+    [previous, current] = [current, previous + current];
+    yieldCount++;
+  }
+}
 
 /**
  * Write a function that creates a simple iterator object.
@@ -60,4 +88,17 @@ export function* fibonacci(n) {}
  * iter.next() // {value: 3, done: false}
  * iter.next() // {value: undefined, done: true}
  */
-export function createIterator(array) {}
+export function createIterator(array) {
+  let index = 0;
+
+  return {
+    next() {
+      if (index < array.length) {
+        index++;
+        return { value: array[index - 1], done: false };
+      } else {
+        return { value: undefined, done: true };
+      }
+    }
+  };
+}
